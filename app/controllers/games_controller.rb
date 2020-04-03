2d460b81d296be885c7cbf754fca71d70e0ad2f3
Josh Morris <jmo0624@gmail.com>
2020-04-03 00:34:51 +0000

    add route to create new game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,11 @@
 class GamesController < ApplicationController
   
+  #render form to create new entry.....'games/new'
+  get '/games/new' do
+    erb :'games/new'
+  end
+  #post games to create new game entry
   
+  #index for all games
   
 end
