6834df0a426d4fd3745cecd2ae9da637c3dd837b
Josh Morris <jmo0624@gmail.com>
2020-04-03 00:53:52 +0000

    add post route for new game and get rout for show page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,7 +5,26 @@ class GamesController < ApplicationController
     erb :'games/new'
   end
   #post games to create new game entry
+  post '/games' do
+    
+    #if !logged_in?
+     # redirect "/"
+   # end
+    
+    if params[:title] != ""
+      #create new entry
+      @game = Game.create(title: params[:title], user_id: current_user.id)
+      redirect "/games/#{@game.id}"
+    else
+      redirect "games/new"
+    end
+      
+  end
   
+  get '/games/:id' do
+    @game = Game.find(params[:id])
+    erb :'/games/show'
+  end
   #index for all games
   
 end
