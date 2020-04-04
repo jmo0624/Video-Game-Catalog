2ab2918095d2a3a73aae701348c4967fa1b781b0
Josh Morris <jmo0624@gmail.com>
2020-04-04 17:23:48 +0000

    add new helper methods and register Sinatra::Flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,26 +7,41 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "secret"
+    register Sinatra::Flash
   end
 
   get "/" do
-    
-    if logged_in?
-      redirect "/users/#{current_user.id}"
-    else
+      
+      redirect_if_logged_in
       erb :welcome
-    end
     
   end
   
   helpers do
     
     def logged_in?
-      !!current_user
+      !!current_user  #true is user is logged in
     end
     
     def current_user
       @current_user ||= User.find_by(id: session[:user_id])
+    end
+    
+    def authorized_to_edit?(game)
+      game.user == current_user
+    end
+    
+    def redirect_if_not_logged_in
+      if !logged_in?
+        flash[:errors] = "You must be logged in to do that."
+        redirect '/'
+      end
+    end
+    
+    def redirect_if_logged_in
+      if logged_in?
+        redirect "/users/#{current_user.id}"
+      end
     end
     
   end
